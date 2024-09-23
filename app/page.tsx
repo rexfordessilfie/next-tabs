4c9002fd6aced673e51a046a2ceba2b6fb7e6299
Rexford Essilfie <rexfordessilfie09@gmail.com>
2024-09-23 01:28:05 -0700

    chore: back to lighter yellow

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import { Counter } from "./components/Counter";
 
 export default function Home() {
   return (
-    <div className="border bg-yellow-500 dark:bg-yellow-700 rounded-md p-2 dark:border-gray-500 gap-2 flex flex-col">
+    <div className="border bg-yellow-500 dark:bg-yellow-600 rounded-md p-2 dark:border-gray-500 gap-2 flex flex-col">
       <h1 className="font-semibold">Home</h1>
       <Counter label="Page" />
     </div>
